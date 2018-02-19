0368cdc993636355b1688b5cdaa53d0a4be78823
Sourabrata Mukherjee <sourabrta.moukherjee@mheducation.com>
2018-02-19 12:49:15 +0530

    Centralized Cloud Configuration Demo
    Centralized Cloud Configuration Demo

---
+++ SpringCloudConfigDemo/src/main/java/com/souro/SpringCloudConfigDemo/App.java
@@ -0,0 +1,13 @@
+package com.souro.SpringCloudConfigDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
